3f4b5682bf288fb810962639c49bb84499f8dc3e
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-09-08 11:44:52 -0300

    Criando ações de publish

---
+++ resources/lang/pt-br/laravel-comet-theme.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'sitename'                   => 'Projeto Comet',
+];
---
+++ src/CometThemeServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Uspdev\CometTheme;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class CometThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(
+        Factory $view,
+        Dispatcher $events,
+        Repository $config
+    ) {
+        $this->loadViews();
+        $this->loadTranslations();
+        $this->publishAssets();
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    private function packagePath($path)
+    {
+        return __DIR__."/../$path";
+    }
+
+    private function loadViews()
+    {
+        $viewsPath = $this->packagePath('resources/views');
+        $this->loadViewsFrom($viewsPath, 'laravel-comet-theme');
+        $this->publishes([
+            $viewsPath => base_path('resources/views/vendor/laravel-comet-theme'),
+        ], 'views');
+    }
+
+    private function loadTranslations()
+    {
+        $translationsPath = $this->packagePath('resources/lang');
+        $this->loadTranslationsFrom($translationsPath, 'laravel-comet-theme');
+        $this->publishes([
+            $translationsPath => base_path('resources/lang/vendor/laravel-comet-theme'),
+        ], 'translations');
+    }
+
+    private function publishAssets()
+    {
+        $this->publishes([
+            $this->packagePath('resources/assets') => public_path('vendor/laravel-comet-theme'),
+        ], 'assets');
+    }
+
+}
