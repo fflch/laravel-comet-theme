43fa14ba2323cf5b15fe4cf9859ac03fce396318
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-12 17:42:47 -0300

    alterado o menu e o topo

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,15 @@
 @section('menu')
-    <ul>
-        <li>Example 1</li>
-        <li>Example 2</li>
-    </ul>
+    <nav class="navbar navbar-expand text-align-center" style="background-color: #FFC76F;" role="navigation">
+        <ul class="nav navbar-nav">
+          <li class="nav-item active divider-vertical">
+            <a class="nav-link" href="#">Busca</a>
+          </li>
+          <li class="nav-item divider-vertical">
+            <a class="nav-link" href="#">O que é</a>
+          </li>
+          <li class="nav-item divider-vertical">
+            <a class="nav-link" href="/corporas/create">Criar Corpora</a>
+          </li>
+        </ul>
+    </nav>
 @show
