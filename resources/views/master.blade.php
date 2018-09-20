8b89c752cd0ba57b1b1aa75a35a92f5177d16d28
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-20 13:50:16 -0300

    ajustado a chamada de JS no master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -49,13 +49,7 @@
         </div>
 
         @section('javascripts')
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.pt-BR.min.js"></script>
-            <script type="text/javascript" src="{{ asset('/vendor/laravel-comet-theme/js/script.js') }}"></script>
+            <script type="text/javascript" src="{{ asset('/js/script.js') }}"></script>
         @show
     </body>
 </html>
