de68ff1bdef2ad268b4cdc17c298e69e1cf1c2f0
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-09-26 17:26:38 -0300

    tentativa de isolar funcoes

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,1 +1,29 @@
-console.log("working");
+(function() {
+
+  var teste = 'teste';
+
+  /**
+   * Show and Hide Elements from their parent
+   * @param {Array} showElms - Array of elements with their querySelector
+   * Example :
+   * for(radio in radios) {
+   *    radios[radio].onclick = function() {
+   *      showHideFields([this.getAttribute("data-show")]);
+   *    }
+   * }
+  */
+
+  var showHideFields = function (showElms){
+    parentElm = document.querySelector(showElms[0]).parentElement;
+    hideFields = Array.from(parentElm.children);
+
+    hideFields.map( function( hideField ) {
+        hideField.classList.add("d-none");
+    } );
+
+    showElms.map( function( showElm ) {
+        document.querySelector(showElm).classList.remove("d-none");
+    } );
+  }
+
+})();
