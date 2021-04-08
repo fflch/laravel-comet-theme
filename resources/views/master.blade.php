fb5979ac724167489a399a7b9da0f279aae28cb6
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-04-08 11:55:09 -0300

    cdn added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,7 +15,21 @@
 
         <!-- CSS -->
         @section('styles')
-            <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
+
+        {{-- Botstrap 4 --}}
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+            {{-- Botstrap 4 --}}
+
+            {{-- FontAwesome --}}
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+            {{-- FontAwesome --}}
+
+            {{-- JqueryUI --}}
+            <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+            {{-- JqueryUI --}}
+
+            <link rel="stylesheet" href="{{ asset('/vendor/laravel-comet-theme/css/styles.css')}}">
+            <link rel="stylesheet" href="{{ asset('/vendor/laravel-comet-theme/css/breakpoints.css')}}">
         @show
     </head>
 
@@ -50,7 +64,28 @@
         </div>
 
         @section('javascripts')
-            <script type="text/javascript" src="{{ asset('/js/script.js') }}"></script>
+
+        {{-- Jquery --}}
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        {{-- Jquery --}}
+
+        {{-- Popper --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        {{-- Popper --}}
+
+        {{-- Botstrap 4 --}}
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        {{-- Botstrap 4 --}}
+
+        {{-- FontAwesome --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+        {{-- FontAwesome --}}
+        
+        {{-- JqueryUI --}}
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        {{-- JqueryUI --}}
+
+        <script type="text/javascript" src="{{ asset('/vendor/laravel-comet-theme/js/script.js') }}"></script>
         @show
     </body>
 </html>
